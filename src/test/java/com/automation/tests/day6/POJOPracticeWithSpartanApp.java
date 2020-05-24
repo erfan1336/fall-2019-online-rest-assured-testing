5b6375162afd0d88f77066a6c70fd0ec3567d443
erfan1336 <erfan1336@gmail.com>
2020-05-23 18:50:57 -0700

    POJOs added

--- src/test/java/com/automation/tests/day6/POJOPracticeWithSpartanApp.java
+++ src/test/java/com/automation/tests/day6/POJOPracticeWithSpartanApp.java
@@ -1,10 +1,12 @@
 package com.automation.tests.day6;
 
+import com.automation.pojos.Spartan;
 import com.automation.utilities.ConfigurationReader;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -60,10 +62,49 @@ public class POJOPracticeWithSpartanApp {
     }
 
     @Test
+    @DisplayName("Retrieve existing username, and update name and verify if update is successfully")
     public void updateSpartanTest(){
 
         int userToUpdate = 306;
+        String name = "Cucumber306";
 
+
+        //HTTP PUT request to update existing record, for example existing Spartan
+        //PUT - requires to provide ALL parameters in body
+
+        Spartan spartan = new Spartan(name,"Male",123456781012L);
+
+        //get spartan from web service
+        Spartan spartanToUpdate = given().
+                                        auth().basic("admin","admin").
+                                        accept(ContentType.JSON).
+                                  when().
+                                        get("/spartans/{id}",userToUpdate).as(Spartan.class);
+
+        //update property that you need without affecting other properties
+        System.out.println("Before update:"+spartanToUpdate);
+        spartanToUpdate.setName(name);
+        System.out.println("After update: " + spartanToUpdate);
+
+        Response response = given().
+                                    auth().basic("admin","admin").
+                                    contentType(ContentType.JSON).
+                                    body(spartan).                        //pass the object into the body of request
+                            when().
+                                    put("/spartans/{id}",userToUpdate).prettyPeek();
+
+
+        response.then().statusCode(204);
+
+
+        System.out.println("#########################################################");
+        //to get user with id 306, the one we just updated
+        given().
+                auth().basic("admin","admin").
+        when().
+                get("/spartans/{id}",userToUpdate).prettyPeek().
+        then().
+                statusCode(200).body("name",is(name));
 
 
 
